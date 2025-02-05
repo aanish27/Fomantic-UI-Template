db98d9d8fc3783f5281480c686cef3551b34f20a
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2025-02-05 15:09:56 -0500

    Integration successfull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,6 +164,12 @@
                                 </div>
                             </div>
                         </div>
+                        <button class="ui primary button">
+                          Save
+                        </button>
+                        <button class="ui button">
+                           Discard
+                        </button>
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
