096f5e41bf9f498bef856605e0a6e8a6350e2976
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2025-02-07 10:02:30 -0500

    style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,50 +7,84 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="">
-        <!-- Sidebar Menu -->
-       <div class="ui left demo vertical visible inverted labeled icon sidebar menu ">
-            <a class="item">
-                <i class="home icon"></i>
-                Home
-            </a>
-            <a class="item">
-                <i class="block layout icon"></i>
-                Topics
-            </a>
-            <a class="item">
-                <i class="smile icon"></i>
-                Friends
-            </a>
-        </div>
+    <body>
+        <div class="ui inverted vertical masthead center aligned segment">
+            <div class="ui container">
+                <div class="ui large secondary inverted pointing menu">
+                    <a class="active item">Home</a>
+                    <a class="item">About</a>
+                    <a class="item">Contact</a>
+                    <div class="right item">
+                        <a class="ui inverted button">Log in</a>
+                        <a class="ui inverted button">Sign Up</a>
+                    </div>
+                </div>
+            </div>
 
-        <div class="ui secondary menu ">
-            <a class="active item">
-                Home
-            </a>
-            <a class="item">
-                Messages
-            </a>
-            <a class="item">
-                Friends
-            </a>
-            <div class="right menu">
-                <div class="item">
-                <div class="ui icon input">
-                    <input type="text" placeholder="Search...">
-                    <i class="search link icon"></i>
+            <div class="ui text container">
+                <h1 class="ui inverted header">
+                    Welcome to Our Website
+                </h1>
+                <h2>Experience the best services with us.</h2>
+                <a class="ui huge primary button" href="#">Get Started <i class="right arrow icon"></i></a>
+            </div>
+        </div>
+        <div class="ui vertical stripe segment">
+            <div class="ui middle aligned stackable grid container">
+                <div class="row">
+                    <div class="eight wide column">
+                        <h3 class="ui header">Our Mission</h3>
+                        <p>We aim to provide the best services to our customers with utmost dedication and professionalism.</p>
+                    </div>
+                    <div class="six wide right floated column">
+
+                    </div>
                 </div>
-                </div>
-                <a class="ui item">
-                Logout
-                </a>
             </div>
         </div>
 
-    <script type="text/javacsript">
-       $(function () {
-            $('.ui.sidebar').sidebar('toggle');
-       });
-    </script>
+        <div class="ui inverted vertical footer segment">
+            <div class="ui container">
+                <div class="ui stackable inverted divided equal height stackable grid">
+                    <div class="three wide column">
+                        <h4 class="ui inverted header">About</h4>
+                        <div class="ui inverted link list">
+                            <a href="#" class="item">Sitemap</a>
+                            <a href="#" class="item">Contact Us</a>
+                            <a href="#" class="item">Religious Ceremonies</a>
+                            <a href="#" class="item">Gazebo Plans</a>
+                        </div>
+                    </div>
+                    <div class="three wide column">
+                        <h4 class="ui inverted header">Services</h4>
+                        <div class="ui inverted link list">
+                            <a href="#" class="item">Banana Pre-Order</a>
+                            <a href="#" class="item">DNA FAQ</a>
+                            <a href="#" class="item">How To Access</a>
+                            <a href="#" class="item">Favorite X-Men</a>
+                        </div>
+                    </div>
+                    <div class="seven wide column">
+                        <h4 class="ui inverted header">Footer Header</h4>
+                        <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(function () {
+                $('.context1 .menu .item')
+                .tab({
+                    context: '.context1'
+                })
+                ;
+                $('.context2 .menu .item')
+                .tab({
+                    // special keyword works same as above
+                    context: 'parent'
+                })
+;
+            });
+        </script>
     </body>
 </html>
