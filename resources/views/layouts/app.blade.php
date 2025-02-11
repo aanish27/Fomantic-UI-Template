cfb6e3f1eafb20baa7b681a65629b49544cd9da9
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2025-02-11 16:51:53 -0500

    dashboard integrated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,30 +7,60 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script defer src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.js"></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body>
+        {{-- sidebar --}}
+        <div class="ui left demo vertical inverted labeled icon  sidebar menu ">
+            <a class="item">
+                <img class="ui centered mini circular image" src="{{ url('images/logo.png') }}">
+            </a>
+            <a class="item">
+                <img class="ui centered mini circular image" src="{{ url('images/profile.jpg') }}">
+            </a>
+            <a class="item"> <i class="user icon"></i> User </a>
+            <a class="item"> <i class="book icon"></i> Books </a>
+            <a class="item"> <i class="globe icon"></i> Explore </a>
+            <a class="item"> <i class="bookmark icon"></i> Bookmark </a>
+        </div>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+        <div class="pusher">
+            {{-- nav --}}
+            <div class="ui secondary menu" style="background-color: whitesmoke; margin-bottom: 0;">
+                <a class="item" id="btn_sidebar"> <i class="hamburger icon"></i> </a>
+                <a class="item"> Home </a>
+                <a class="item"> Contact </a>
+                <div class="right menu">
+                    <a class="item"> <i class="search icon"></i> </a>
+                    <a class="item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> <i class="sign out alternate icon"></i></i></a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </div>
 
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+
+            <div class="ui inverted vertical footer segment center aligned">
+                <p>Copyright © 2019 - 2025 VistaG. All rights reserved.</p>
+            </div>
         </div>
+
+    <script type="module">
+        $(function () {
+            $('#btn_sidebar').on('click', function () {
+                $('.ui.labeled.icon.sidebar').sidebar('toggle');
+            });
+
+            $('.ui.labeled.icon.sidebar').sidebar({
+                context: $('body'),
+                dimPage: false,
+                closable: false
+            }).sidebar('attach events', '.sidebar-toggle').sidebar('show')
+        });
+    </script>
     </body>
 </html>
